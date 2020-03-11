ed16c2e4abf501671098e4e84d62f5f9b98288e9
Ryan <ryanimpey@live.co.uk>
2020-03-11 17:21:41 +0000

    Improved the formatting into a readable state

--- assignment/assignment.cpp
+++ assignment/assignment.cpp
@@ -24,14 +24,14 @@ int main(int argc, char **argv) {
 	int device_id = 0;
 
 	// Load in our initial reference file
-	string image_filename = "test.pgm";
+	string inputImgFilename = "test.pgm";
 
 	// Handle command line arguements
 	for (int i = 1; i < argc; i++) {
 		if ((strcmp(argv[i], "-p") == 0) && (i < (argc - 1))) { platform_id = atoi(argv[++i]); }
 		else if ((strcmp(argv[i], "-d") == 0) && (i < (argc - 1))) { device_id = atoi(argv[++i]); }
 		else if (strcmp(argv[i], "-l") == 0) { std::cout << ListPlatformsDevices() << std::endl; }
-		else if ((strcmp(argv[i], "-f") == 0) && (i < (argc - 1))) { image_filename = argv[++i]; }
+		else if ((strcmp(argv[i], "-f") == 0) && (i < (argc - 1))) { inputImgFilename = argv[++i]; }
 		else if (strcmp(argv[i], "-h") == 0) { print_help(); return 0; }
 	}
 
@@ -41,25 +41,25 @@ int main(int argc, char **argv) {
 	//detect any potential exceptions
 	try {
 		// Returns a pointer to a image location from its filename
-		CImg<unsigned char> image_input(image_filename.c_str());
+		CImg<unsigned char> inputImgPtr(inputImgFilename.c_str());
 
 		// Select platform and device to use to create a context from
 		cl::Context context = GetContext(platform_id, device_id);
 
-		//display the selected device
+		// Display the selected device
 		std::cout << "Running on " << GetPlatformName(platform_id) << ", " << GetDeviceName(platform_id, device_id) << std::endl;
 
-		//create a queue to which we will push commands for the device
+		// Create a queue to which we will push commands for the device
 		cl::CommandQueue queue(context);
 
-		//3.2 Load & build the device code
+		// Create a program to combine context and kernels
 		cl::Program::Sources sources;
 		AddSources(sources, "kernels/my_kernels.cl");
 
 		// Create a program to combine context and kernels
 		cl::Program program(context, sources);
 
-		// Attempt to build the program
+		// Attempt to build the OpenCL Program and catch any errors that occur during build
 		try { 
 			program.build();
 		}
@@ -70,74 +70,76 @@ int main(int argc, char **argv) {
 			throw err;
 		}
 
-		//Part 4 - device operations
+		/* Part 1 - Histogram Generation */
 
 		// Create a host vector (Histogram Bin) to hold our output values
-		std::vector<int> bin(256);
-		size_t bin_size = bin.size() * sizeof(int);
+		std::vector<int> histBin(256);
+		size_t histBinSize = histBin.size() * sizeof(int);
 
-		// Create two buffers, one for the input image and one for the output
-		cl::Buffer image_input_buffer(context, CL_MEM_READ_ONLY, image_input.size());
-		////cl::Buffer image_output_buffer(context, CL_MEM_READ_ONLY, image_input.size());
+		// Create our buffers for usage in OpenCL Kernels
+		cl::Buffer inputImgBuffer(context, CL_MEM_READ_ONLY, inputImgPtr.size()); // Create a read-only buffer with a size of our input image
+		cl::Buffer histBuffer(context, CL_MEM_READ_WRITE, histBinSize);  // Create a read-write buffer with the size of our histogram bin (bin_size * size(int))
+//		cl::Buffer image_output_buffer(context, CL_MEM_READ_ONLY, inputImgPtr.size()); // Not in use yet!
+//		cl::Buffer scan_histogram_buffer(context, CL_MEM_READ_WRITE, histBinSize); // Not in use yet!
 
-		// Create a third bin to hold our histogram values
-		cl::Buffer image_histogram_buffer(context, CL_MEM_READ_WRITE, bin_size);
-		cl::Buffer scan_histogram_buffer(context, CL_MEM_READ_WRITE, bin_size);
+		std::cout << "Bin Size: " << histBin.size() << std::endl;
+		std::cout << "Bin Size in Bytes: " << histBinSize << std::endl;
 
-		std::cout << "Bin Size: " << bin.size() << std::endl;
-		std::cout << "Bin Size in Bytes: " << bin_size << std::endl;
+		// Write image input data to our device's memory via our image input buffer
+		queue.enqueueWriteBuffer(inputImgBuffer, CL_TRUE, 0, inputImgPtr.size(), &inputImgPtr.data()[0]);
+		// Write histogram bin buffer filled with 0's to our device's memory
+		queue.enqueueFillBuffer(histBuffer, 0, 0, histBinSize);
 
-		// Copy input buffer to device memory
-		queue.enqueueWriteBuffer(image_input_buffer, CL_TRUE, 0, image_input.size(), &image_input.data()[0]);
+		// Set up histogram kernel for device execution
+		cl::Kernel kernel_histogram = cl::Kernel(program, "histogram"); // Load the histogram kernel defined in my_kernels
+		kernel_histogram.setArg(0, inputImgBuffer);  // Pass in our image buffer as our input
+		kernel_histogram.setArg(1, histBuffer);  // Pass in our histogram buffer as our output
 
-		// Fill histogram bin buffer with 0's on device memory
-		queue.enqueueFillBuffer(image_histogram_buffer, 0, 0, bin_size);
+		// Execute our histogram kernel on the selected device
+		queue.enqueueNDRangeKernel(kernel_histogram, cl::NullRange, cl::NDRange(inputImgPtr.size()), cl::NullRange);
 
-		// Set up kernel for device execution and pass in buffers as arguements
-		cl::Kernel kernel_histogram = cl::Kernel(program, "histogram");
-		kernel_histogram.setArg(0, image_input_buffer);
-		kernel_histogram.setArg(1, image_histogram_buffer);
+		// Write the histogram result from our device memory to our vector via the histogram buffer
+		queue.enqueueReadBuffer(histBuffer, CL_TRUE, 0, histBinSize, &histBin[0]);
 
-		// Enqueues a command to execute a kernel on a device
-		queue.enqueueNDRangeKernel(kernel_histogram, cl::NullRange, cl::NDRange(image_input.size()), cl::NullRange);
-
+		std::cout << "Histogram Bin: " << histBin << "\n\n" << std::endl;
 
 		// Create an output buffer to store values copied from device once computation is complete
-		////vector<unsigned char> output_buffer(image_input.size());
+		////vector<unsigned char> output_buffer(inputImgPtr.size());
 
 		// Copy result to the buffer just defined
 		////queue.enqueueReadBuffer(image_output_buffer, CL_TRUE, 0, output_buffer.size(), &output_buffer.data()[0]); // Not needed yet
-		// Copy result back to our original 'bin' vector
-		queue.enqueueReadBuffer(image_histogram_buffer, CL_TRUE, 0, bin_size, &bin[0]);
+		
 
-		// ---
+		/* PART TWO - CUMULATIVE HISTOGRAM */
 
-		std::cout << "Histogram Bin: " << bin << "\n\n" << std::endl;
+		
 
-		std::vector<int> bin_test(256, 1);
+		//std::vector<int> bin_test(256, 1);
 
-		std::cout << "Bin Test:" << bin_test << "\n" << std::endl;
+		//std::cout << "Bin Test:" << bin_test << "\n" << std::endl;
 
-		// Fill histogram bin buffer with 0's on device memory
-		queue.enqueueFillBuffer(scan_histogram_buffer, 0, 0, bin_size);
-		queue.enqueueFillBuffer(image_histogram_buffer, 0, 0, bin_size);
 
-		queue.enqueueWriteBuffer(image_histogram_buffer, CL_TRUE, 0, bin_test.size(), &bin_test[0]);
+		//// Fill histogram bin buffer with 0's on device memory
+		//queue.enqueueFillBuffer(scan_histogram_buffer, 0, 0, histBinSize);
 
-		// Redefine the kernel as our second step
-		cl::Kernel kernel_scan = cl::Kernel(program, "scan_hs");
-		kernel_scan.setArg(0, image_histogram_buffer);
-		kernel_scan.setArg(1, scan_histogram_buffer);
+		//queue.enqueueWriteBuffer(histBuffer, CL_TRUE, 0, bin_test.size() * sizeof(int), &bin_test[0]);
 
-		// Enqueues a command to execute a kernel on a device
-		queue.enqueueNDRangeKernel(kernel_scan, cl::NullRange, cl::NDRange(bin_size), cl::NDRange(256));
+		//// Redefine the kernel as our second step
+		//cl::Kernel kernel_scan = cl::Kernel(program, "scan_add");
+		//kernel_scan.setArg(0, histBuffer);
+		//kernel_scan.setArg(1, scan_histogram_buffer);
+		//kernel_scan.setArg(2, cl::Local(bin_test.size() * sizeof(int)));//local memory size
+		//kernel_scan.setArg(3, cl::Local(bin_test.size() * sizeof(int)));//local memory size
 
-		queue.enqueueReadBuffer(scan_histogram_buffer, CL_TRUE, 0, bin_test.size(), &bin[0]);
+		//// Enqueues a command to execute a kernel on a device
+		//queue.enqueueNDRangeKernel(kernel_scan, cl::NullRange, cl::NDRange(histBinSize), cl::NDRange(256));
 
-		std::cout << "\nCumulative Bin: " << bin << std::endl;
+		//queue.enqueueReadBuffer(scan_histogram_buffer, CL_TRUE, 0, bin_test.size(), &bin[0]);
+
+		//std::cout << "\nCumulative Bin: " << bin << std::endl;
 
 		// Create a new CImg and window to display the results
-		////CImg<unsigned char>image_output(output_buffer.data(), image_input.width(), image_input.height(), image_input.depth(), image_input.spectrum());
+		////CImg<unsigned char>image_output(output_buffer.data(), inputImgPtr.width(), inputImgPtr.height(), inputImgPtr.depth(), inputImgPtr.spectrum());
 		////CImgDisplay disp_output(image_output, "output");
 
 		// Requires both the input and output image to be closed before the application is terminated
